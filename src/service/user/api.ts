9ab10c0929a9f861123ccd217ccad407474ae574
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2024-10-05 16:30:40 +0900

    tryrt

--- src/service/user/api.ts
+++ src/service/user/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { User } from "../../types/type"
-const BASE_URL=import.meta.env.VITE_API_BASE_URL
+const BASE_URL=import.meta.env.VITE_API_BASE_URL || "http://express-app-service.express.svc.cluster.local/"
 const axiosInstance = axios.create({baseURL:BASE_URL})
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -8,7 +8,7 @@ type ResultGet={
     rows:User[]
 }
 export const isExistUserByEmail=async(email:string)=>{
-    console.log("baseutl",import.meta.env)
+    console.log("baseutl",BASE_URL)
     //return (await axiosInstance.get<User[]>(`users/${email}`)).data
     return (await axiosInstance.get<ResultGet>(`users?email=${email}`)).data.rows.length > 0
 
