b05a59fc73f654f55547a90d5fe879c0c095ba93
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2024-10-05 15:11:45 +0900

    asd

--- src/service/user/api.ts
+++ src/service/user/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { User } from "../../types/type"
-const BASE_URL="http://localhost:5002/"
+const BASE_URL=import.meta.env.VITE_API_BASE_URL
 const axiosInstance = axios.create({baseURL:BASE_URL})
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
