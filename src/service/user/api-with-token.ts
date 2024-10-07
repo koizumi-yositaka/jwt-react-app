44901eb1672c4459d5a39be30998f63ea6a5f69f
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2024-10-08 02:07:11 +0900

    sido

--- src/service/user/api-with-token.ts
+++ src/service/user/api-with-token.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL=import.meta.env.VITE_API_BASE_URL || "http://localhost:80/"
+const BASE_URL=import.meta.env.VITE_API_BASE_URL
 const axiosInstance = axios.create({baseURL:BASE_URL+"hoge"})
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
--- src/service/user/api.ts
+++ src/service/user/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 axios.defaults.withCredentials = true;
-const BASE_URL=import.meta.env.VITE_API_BASE_URL || "http://localhost:80/"
+const BASE_URL=import.meta.env.VITE_API_BASE_URL
 const axiosInstance = axios.create({baseURL:BASE_URL})
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
