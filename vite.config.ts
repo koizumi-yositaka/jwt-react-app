934f4c71f916aff82fd1c511657e65a13660407c
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2024-10-20 23:25:35 +0900

    testの追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import {TanStackRouterVite} from "@tanstack/router-vite-plugin"
+import dynamicImport from "vite-plugin-dynamic-import";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(),TanStackRouterVite()],
+// })
+
 export default defineConfig({
-  plugins: [react(),TanStackRouterVite()],
-})
+  plugins: [react(), dynamicImport(),TanStackRouterVite()],
+  test: {
+    //  describe, expect, it などのAPIをファイル内でimportしなくても使用可にする設定
+    globals: true,
+    // dom
+    environment: "happy-dom",
+    // テスト全体で使用するライブラリをvitest-setup.jsに記載し、インポートする設定
+    setupFiles: [resolve(__dirname, "src", "vitest-setup.js")],
+  },
+});
