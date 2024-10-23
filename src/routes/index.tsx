0b663185c6289edede3813f7809f756b58b32abf
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2024-10-24 01:45:31 +0900

    change message

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-  component: () => <div>Hello World!!!</div>,
+  component: () => <div>こんにちは！</div>,
 })
